1975a0edfb7c73b6ce7eb335f549cb1d297504be
kakacy <364715205@qq.com>
2020-09-07 19:26:05 +0800

    程序员A第七次提交

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
 fs.readFile('./data.txt', {encoding:'utf8'}, (err, data)=> {
     if(!err) {
         console.log(data);
